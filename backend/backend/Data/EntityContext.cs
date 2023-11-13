273df85ecf67ac4b1ffc7aecf569db9f1f0741b6
matthieurobert <matthieurobert82@gmail.com>
2023-11-13 14:20:47 +0100

    feat: add database connection

---
+++ backend/backend/Data/EntityContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class EntityContext : DbContext
+{
+    protected readonly IConfiguration _configuration;
+
+    public EntityContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DatabaseConnection"));
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,6 +1,13 @@
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+
 // Add services to the container.
+builder.Services.AddDbContext<EntityContext>(opt => 
+    opt.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
