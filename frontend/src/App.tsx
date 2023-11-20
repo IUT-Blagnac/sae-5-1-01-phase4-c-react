ef717194366aac9adf5c9f68f6a4a1de0a5a5d7f
Eric-Philippe <ericphlpp@gmail.com>
2023-11-20 23:17:24 +0100

    Create Login Page (FRONT)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import React from "react";
+import SignInSide from "./pages/Login";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
+  const defaultTheme = createTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <SignInSide />
+    </ThemeProvider>
   );
 }
 
