b2da821d94be4d40ff83211389f133eb671ceac6
Sonixray <5Q?AMc@6!X7XxyioDQ!x>
2023-11-21 15:02:34 +0100

    #10 init class data models

---
+++ backend/backend/Data/Models/Equipe.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Data.Models
+{
+    [Table("equipe")]
+    public class Equipe
+    {
+    }
+}
---
+++ backend/backend/Data/Models/Qualite.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Data.Models
+{
+    [Table("qualite")]
+    public class Qualite
+    {
+    }
+}
