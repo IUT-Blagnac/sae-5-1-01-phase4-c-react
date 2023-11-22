1820bd3cb2faafebaa585ae964aa24499a3554b3
matthieurobert <matthieurobert82@gmail.com>
2023-11-22 17:09:26 +0100

    feat: add service and controller for Teams

--- backend/backend/Data/Models/Team.cs
+++ backend/backend/Data/Models/Team.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 
 namespace backend.Data.Models
 {
@@ -10,14 +9,11 @@ namespace backend.Data.Models
         public string name { get; set; }
         public string color { get; set; }
         
-        [JsonIgnore]
         public List<UserTeam> user_team { get; set; }
-        [JsonIgnore]
         public List<Challenge> creator_challenge { get; set; }
-        [JsonIgnore]
         public List<Challenge> target_challenge { get; set; }
         
-        public List<TeamWish> wish { get; set; }
+        public List<Wish> wish { get; set; }
         public List<TeamSubject> team_subject { get; set; }
     }
 }
