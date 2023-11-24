72f87f117dfea6fdff2d0318d3430fd13ff21e53
matthieurobert <matthieurobert82@gmail.com>
2023-11-24 10:22:49 +0100

    add missing skil service interface

---
+++ backend/backend/Services/Interfaces/ISkillService.cs
@@ -0,0 +1,12 @@
+﻿using backend.Data.Models;
+
+namespace backend.Services.Interfaces;
+
+public interface ISkillService
+{
+    public List<Skill> GetSkills();
+    public Skill GetSkillById(Guid id);
+    public Skill AddSkill(string name);
+    public void RemoveSkill(Guid id);
+
+}
