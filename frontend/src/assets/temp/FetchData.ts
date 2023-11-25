1b5c62a4658a8e4ac3638e0728e95c6a7cd21685
Eric-Philippe <ericphlpp@gmail.com>
2023-11-25 16:01:17 +0100

    Add id property to Topic interface and set default value in NewTopic component

--- frontend/src/assets/temp/FetchData.ts
+++ frontend/src/assets/temp/FetchData.ts
@@ -1,5 +1,8 @@
 import Sae from "../../models/Sae";
+import Topic from "../../models/Topic";
 import { SAEStatus } from "../enums/SAEStatus.enum";
+
+const TIMEOUT = 400;
 
 /**
  * @deprecated
@@ -9,20 +12,41 @@ import { SAEStatus } from "../enums/SAEStatus.enum";
  * It is used to test the frontend without having to wait for the backend to be ready.
  */
 export default class FetchData {
-  static async fetchSae(): Promise<Sae[]> {
+  static async fetchSaes(): Promise<Sae[]> {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve([SAE_ONE, SAE_TWO]);
-      }, 1500);
+        resolve(SAEs);
+      }, TIMEOUT);
+    });
+  }
+
+  static async fetchSae(id: string): Promise<Sae> {
+    const saeFound = SAEs.find((sae) => sae.id === id) as Sae;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(saeFound);
+      }, TIMEOUT);
+    });
+  }
+
+  static async fetchTopics(id: string): Promise<Topic[]> {
+    const topicFound = LINK_TOPIC_SAE.find((link) => link.sae === id) as {
+      sae: string;
+      topic: Topic[];
+    };
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(topicFound.topic);
+      }, TIMEOUT);
     });
   }
 }
 
 const SAE_ONE: Sae = {
   id: "1",
-  name: "Koh-Lanta",
-  description: "Useless",
-  statut: SAEStatus.LAUNCHED,
+  name: "Koh-Lanta Pending Users",
+  description: "Koh-Lanta description pas super utile en vraie",
+  statut: SAEStatus.PENDING_USERS,
   min_student_per_group: 1,
   max_student_per_group: 2,
   min_group_per_subject: 1,
@@ -31,11 +55,61 @@ const SAE_ONE: Sae = {
 
 const SAE_TWO: Sae = {
   id: "2",
-  name: "Slaves Narratives",
-  description: "Say no to slavery",
-  statut: SAEStatus.PENDING_USERS,
+  name: "Koh-Lanta Pending Wishes",
+  description: "Koh-Lanta description pas super utile en vraie",
+  statut: SAEStatus.PENDING_WISHES,
   min_student_per_group: 1,
   max_student_per_group: 2,
   min_group_per_subject: 1,
   max_group_per_subject: 2,
 };
+
+const SAE_THREE: Sae = {
+  id: "3",
+  name: "Koh-Lanta Launched",
+  description: "Koh-Lanta description pas super utile en vraie",
+  statut: SAEStatus.LAUNCHED,
+  min_student_per_group: 1,
+  max_student_per_group: 2,
+  min_group_per_subject: 1,
+  max_group_per_subject: 2,
+};
+
+const SAEs = [SAE_ONE, SAE_TWO, SAE_THREE];
+
+const TOPIC_ONE: Topic = {
+  id: "1",
+  name: "Slaves-Narratives",
+  description: "Say no to slavery",
+  categories: ["PHP", "Refactor"],
+};
+
+const TOPIC_TWO: Topic = {
+  id: "2",
+  name: "Site Vacataires",
+  description:
+    "Salut les vacataires, je suis une longue description, j'ai plus d'idée",
+  categories: ["Tech au Choix", "From Scratch"],
+};
+
+const TOPIC_THREE: Topic = {
+  id: "3",
+  name: "Irit",
+  description: "J'ai plus d'idée",
+  categories: ["Angular", "Refactor"],
+};
+
+const LINK_TOPIC_SAE = [
+  {
+    sae: "1",
+    topic: [TOPIC_ONE, TOPIC_TWO, TOPIC_THREE],
+  },
+  {
+    sae: "2",
+    topic: [TOPIC_ONE, TOPIC_TWO],
+  },
+  {
+    sae: "3",
+    topic: [TOPIC_ONE, TOPIC_THREE],
+  },
+];
