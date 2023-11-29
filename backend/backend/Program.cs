8a4c3a83289017208e458aeb00af1c080bf27b75
Matthieu Robert <matthieurobert82@gmail.com>
2023-11-29 10:33:59 +0100

    feat: add new origin to cors

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         builder => 
         {
-            builder.WithOrigins("http://sae.mrobert.fr", "http://localhost")
+            builder.WithOrigins("http://sae.mrobert.fr", "http://localhost", "http://localhost:3000")
                 .AllowAnyMethod()
                 .AllowAnyHeader();
         });
