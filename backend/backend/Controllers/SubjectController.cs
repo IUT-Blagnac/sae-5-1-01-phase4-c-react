96afbaa35587718e1bc1346bba3bea9cd8be87b9
matthieurobert <matthieurobert82@gmail.com>
2023-12-05 13:04:11 +0100

    feat: create subject service and controller

---
+++ backend/backend/Controllers/SubjectController.cs
@@ -0,0 +1,32 @@
+﻿using backend.Data.Models;
+using backend.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SubjectController : ControllerBase
+{
+    private readonly ISubjectService _subjectService;
+
+    public SubjectController(ISubjectService subjectService)
+    {
+        _subjectService = subjectService;
+    }
+
+    [HttpGet("sae/{id}")]
+    [Authorize]
+    public ActionResult<List<Subject>> GetSubjectBySaeId(Guid id)
+    {
+        var subjects = _subjectService.GetSubjectsBySaeId(id);
+
+        if (subjects == null)
+        {
+            return NotFound();
+        }
+
+        return subjects;
+    }
+}
