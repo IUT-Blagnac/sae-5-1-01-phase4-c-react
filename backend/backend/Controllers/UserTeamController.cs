af64f7e1a2635e1649b6f086ac3fe97f433371f0
Hugo-CASTELL <hugo.castell@outlook.fr>
2023-12-10 09:16:46 +0100

    Fix Authorize attribute and removed [Route()] for [Http...("route")]

--- backend/backend/Controllers/UserTeamController.cs
+++ backend/backend/Controllers/UserTeamController.cs
@@ -1,6 +1,8 @@
 ﻿using backend.Data.Models;
 using backend.FormModels;
 using backend.Services.Interfaces;
+using backend.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
@@ -10,17 +12,18 @@ namespace backend.Controllers;
 public class UserTeamController : ControllerBase
 {
     private readonly IUserTeamService _userTeamService;
-    
+
     public UserTeamController(IUserTeamService userTeamService)
     {
         _userTeamService = userTeamService;
     }
-    
+
     [HttpPost]
+    [Authorize(Roles = RoleAccesses.Teacher)]
     public ActionResult<List<UserTeam>> GenTeam(GenTeamForm genTeamForm)
     {
         var userTeams = _userTeamService.GenTeams(genTeamForm.id_users, genTeamForm.id_teams);
 
         return userTeams;
-    } 
+    }
 }
