06ce48ae76a672eb19dc2de5d6c54ebe9fde84fd
matthieurobert <matthieurobert82@gmail.com>
2023-12-10 16:55:22 +0100

    feat: add route to create groups

---
+++ backend/backend/FormModels/GroupForm.cs
@@ -0,0 +1,7 @@
+﻿namespace backend.FormModels;
+
+public class GroupForm
+{
+    public string name { get; set; }
+    public bool is_apprenticeship { get; set; }
+}
--- backend/backend/Services/Interfaces/IGroupService.cs
+++ backend/backend/Services/Interfaces/IGroupService.cs
@@ -1,9 +1,9 @@
 ﻿using backend.Data.Models;
 
-namespace backend.Services.Interfaces
-{
+namespace backend.Services.Interfaces;
+
 public interface IGroupService
 {
         public List<Group> GetGroups();
-    }
+        public Group CreateGroup(string name, bool is_apprenticeship);
 }
