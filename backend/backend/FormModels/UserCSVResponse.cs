08ed4108b592b72e794fd4a3d8b34c3cfb2c9774
Hugo-CASTELL <hugo.castell@outlook.fr>
2023-12-10 17:18:40 +0100

    API: Fix CSV route

---
+++ backend/backend/FormModels/UserCSVResponse.cs
@@ -0,0 +1,18 @@
+﻿using backend.Data.Models;
+
+namespace backend.FormModels;
+
+public class UserCSVResponse : UserCSVRegister
+{
+    public UserCSVResponse(User user)
+    {
+        FirstName = user.first_name;
+        LastName = user.last_name;
+        Email = user.email;
+        Password = user.password;
+    }
+
+    public string Password { get; set; }
+
+
+}
