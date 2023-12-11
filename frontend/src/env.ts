b06aa80806a3d9156fad129a262dd14fbd818ce6
Eric-Philippe <ericphlpp@gmail.com>
2023-12-11 21:33:27 +0100

    Env

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-const API_URL = "http://localhost:8080";
+const API_URL = process.env.REACT_APP_API_URL || "https://api.mvallew.com";
 const ENV = "dev";
 
 export default API_URL;
